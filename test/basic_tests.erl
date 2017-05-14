687ed6d38e438f767768a4eb54427c83d16ed433
atrolla <yvan.vu@arolla.fr>
2017-05-14 21:44:45 +0200

    basic done

--- test/basic_tests.erl
+++ test/basic_tests.erl
@@ -1,5 +1,5 @@
-%% Compile (when in root/parent dir) this with :
-%% > c("test/basic_tests").
+%% run this command to compile all needed modules and to run tests
+%% >  c("test/basic_tests"), c("src/basic"), eunit:test(basic_tests, [verbose]).
 -module(basic_tests).
 
 %% API
@@ -8,58 +8,52 @@
 %% Include this lib so you can do tests
 -include_lib("eunit/include/eunit.hrl").
 
-%% run all tests with :
-%% > eunit:test(basic_tests,[verbose]).
 a_test_function_ends_with_test() ->
-  ActualValue = correct_this_value,
+  ActualValue = true,
   ExpectedValue = true,
   ?assertEqual(ExpectedValue, ActualValue).
 
-%% run this command to do all in one-liner
-%% (basically runs all commands procedurally)
-%% >  c("test/basic_tests"), c("src/basic"), eunit:test(basic_tests, [verbose]).
-%%
 %% add 't' iteratively at the end of the functions so they are detected as tests
-divide_should_return_the_division_of_two_numbers__tes() ->
+divide_should_return_the_division_of_two_numbers__test() ->
   Dividend = 5,
   Divisor = 2,
   Quotient = 2.5,
   ?assertEqual(Quotient, basic:divide(Dividend, Divisor)).
 
 %% 3
-divide_should_return_undefined_when_divisor_is_0__tes() ->
+divide_should_return_undefined_when_divisor_is_0__test() ->
   Dividend = 5,
   Divisor = 0,
   Quotient = undefined,
   ?assertEqual(Quotient, basic:divide(Dividend, Divisor)).
 
 %% 4
-sum_should_return_the_sum_of_numbers__tes() ->
+sum_should_return_the_sum_of_numbers__test() ->
   A = 3,
   B = 4,
   ?assertEqual(7, basic:sum(A, B)).
 
 %% 5
-sum_should_return_the_sum_of_two_3D_vectors__tes() ->
+sum_should_return_the_sum_of_two_3D_vectors__test() ->
   A = {5, 3, 6},
   B = {1, 2, 3},
   ?assertEqual({6, 5, 9}, basic:sum(A, B)).
 
 %% 6
-sum_should_return_the_sum_all_elements_of_a_list__tes() ->
+sum_should_return_the_sum_all_elements_of_a_list__test() ->
   L = [1, 2, 3, 4, 5],
   ?assertEqual(15, basic:sum(L)).
 
 %% 7
 %% apply with '+' or '/' for case of
-apply_should_return_the_sum_of_2_elements_when_operator_is_plus__tes() ->
+apply_should_return_the_sum_of_2_elements_when_operator_is_plus__test() ->
   Operator = '+',
   ?assertEqual(8, basic:apply(4,Operator, 4)).
 
 %% 8
-apply_should_return_the_division_of_2_elements_when_operator_is_slash__tes() ->
+apply_should_return_the_division_of_2_elements_when_operator_is_slash__test() ->
   Operator = '/',
-  ?assertEqual(1, basic:apply(4,Operator, 4)).
+  ?assertEqual(1.0, basic:apply(4,Operator, 4)).
 
 maps_example_test() ->
   EmptyMap = #{},
