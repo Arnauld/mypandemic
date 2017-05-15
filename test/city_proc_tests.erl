990e75cf81758ecb3791f927f98267f4a6c44a9c
atrolla <yvan.vu@arolla.fr>
2017-05-15 21:51:27 +0200

    changed protocol in proc

--- test/city_proc_tests.erl
+++ test/city_proc_tests.erl
@@ -9,8 +9,8 @@ city_should_start_and_responds_to_infection_level_message__test() ->
   {ok, Pid} = city_proc:start_link(paris, [london, essen]),
   Pid ! {infection_level, blue, self()},
   receive
-    {ok, Level} ->
-      ?assertEqual(0, Level)
+    Response ->
+      ?assertEqual({infection_level, paris, blue, 0}, Response)
   after
     500 ->
       error(timeout)
@@ -19,16 +19,16 @@ city_should_start_and_responds_to_infection_level_message__test() ->
 
 city_should_start_and_expose_its_infection_level__tes() ->
   {ok, Pid} = city_proc:start_link(paris, [london, essen]),
-  Level = city_proc:infection_level(Pid, blue),
-  ?assertEqual(0, Level).
+  Response = city_proc:infection_level(Pid, blue),
+  ?assertEqual({infection_level, paris, blue, 0}, Response).
 
 
 city_should_start_and_responds_to_infect_message__tes() ->
   {ok, Pid} = city_proc:start_link(paris, [london, essen]),
   Pid ! {infect, blue, self()},
   receive
-    {ok, InfectionResult} ->
-      ?assertEqual({infected, paris}, InfectionResult)
+    InfectionResult ->
+      ?assertEqual({infected, paris, blue, 1}, InfectionResult)
   after
     500 ->
       error(timeout)
@@ -38,23 +38,17 @@ city_should_start_and_responds_to_infect_message__tes() ->
 city_should_start_and_be_infected__tes() ->
   {ok, Pid} = city_proc:start_link(paris, [london, essen]),
   InfectionResult = city_proc:infect(Pid, blue),
-  ?assertEqual({infected, paris}, InfectionResult).
+  ?assertEqual({infected, paris, blue, 1}, InfectionResult).
 
-
-%% for tests purpose, we can tell the process no to reply,
-%% the behaviour of the response can be coded like this :
-%% replyTo(no_reply, _Response) -> ok;
-%% replyTo(From, Result) ->
-%%  From ! {ok, Result}.
 city_should_start_and_responds_to_infect_message_until_outbreak__tes() ->
   {ok, Pid} = city_proc:start_link(paris, [london, essen]),
-  Pid ! {infect, blue, no_reply},
-  Pid ! {infect, blue, no_reply},
-  Pid ! {infect, blue, no_reply},
+  Pid ! {infect, blue}, %% async -> no reply
+  Pid ! {infect, blue},
+  Pid ! {infect, blue},
   Pid ! {infect, blue, self()},
   receive
-    {ok, InfectionResult} ->
-      ?assertEqual({outbreak, [london, essen]}, InfectionResult)
+    InfectionResult ->
+      ?assertEqual({outbreak, paris, blue, [london, essen]}, InfectionResult)
   after
     500 ->
       error(timeout)
@@ -67,7 +61,7 @@ city_should_start_and_should_outbreak_after_4_infections__tes() ->
   city_proc:infect(Pid, blue),
   city_proc:infect(Pid, blue),
   OutbreakResult = city_proc:infect(Pid, blue),
-  ?assertEqual({outbreak, [london, essen]}, OutbreakResult).
+  ?assertEqual({outbreak, paris, blue, [london, essen]}, OutbreakResult).
 
 
 each_city_should_start_in_its_own_process__tes() ->
