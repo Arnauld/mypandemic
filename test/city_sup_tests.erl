a684da89d1fe0af741cd43ae1e9bcd3c143d8cda
atrolla <yvan.vu@arolla.fr>
2017-05-16 20:33:36 +0200

    updated city_sup tests

--- test/city_sup_tests.erl
+++ test/city_sup_tests.erl
@@ -10,52 +10,55 @@
 %%   {ok, Pid} = city_proc:start(Name, Neighbours),
 %%   register(Name, Pid),
 %%   monitor(process, Pid).
-supervisor_should_start_and_register_new_city_process__test() ->
+supervisor_should_start_and_register_new_city_process__tes() ->
   {ok, Pid} = city_sup:start(paris, [london, essen]),
   timer:sleep(50),
   try
-    ?assertEqual({paris, blue, 0}, city_proc:infection_level(paris, blue))
+    %% a process should be bound to 'paris'
+    ParisPid = whereis(paris),
+    ?assertEqual(true, is_pid(ParisPid)),
+    ?assertEqual({paris, blue, 0}, city_proc:infection_level(paris, blue)),
+    error_logger:info_msg("Test done~n")
   after
-    io:format("0...~n"),
-    kill_city_sup(Pid, paris)
+    stop_process(Pid, kill),
+    stop_process(paris, kill)
   end.
-
 
 %% a monitored process sends a message of the form :
 %% {'DOWN', Ref, process, Pid, Reason}
 %% before terminating
 %% Reason = normal when it is not a failure
-supervisor_should_restart_the_city_process_after_a_failure__test() ->
+supervisor_should_restart_the_city_process_after_a_failure__tes() ->
   {ok, Pid} = city_sup:start(london, [paris, essen]),
   timer:sleep(50),
   try
-    ?assertEqual({infected, london, blue, 1}, city_proc:infect(london, blue))
+    city_proc:infect(london, blue),
+    stop_process(london, oops),
+
+    %% gives some time for the process to be respawned by supervisor
+    timer:sleep(100),
+    %% a process should be bound to 'paris'
+    LondonPid = whereis(london),
+    ?assertEqual(true, is_pid(LondonPid)),
+    Infection = city_proc:infection_level(london, blue),
+    ?assertEqual({london, blue, 0}, Infection),
+    error_logger:info_msg("Test 2 done~n")
   after
-    kill_city_sup(Pid, london)
+    stop_process(Pid, kill),
+    stop_process(london, kill)
   end.
 
-%% kill -> infections data should be lost
-%timer:sleep(50),
-%kill_city(london),
-%timer:sleep(50),
-%%  ?assertEqual({london, blue, 1}, city_proc:infection_level(london, blue)).
-
-supervisor_should_stop_when_its_child_stops_gracefully__test() ->
+supervisor_should_stop_when_its_child_stops_gracefully__tes() ->
   {ok, Pid} = city_sup:start(essen, [london, paris]),
   timer:sleep(50),
   essen ! stop,
   timer:sleep(50),
   ?assertEqual(false, is_process_alive(Pid)).
 
-kill_city_sup(Pid, City) ->
-  try
-    io:format("1...~n"),
-    exit(Pid, kill),
-    io:format("2...~n"),
-    try exit(whereis(City), kill)
-    catch _ ->
-      ignore end,
-    io:format("3...~n")
-  catch _ ->
-    ignore
-  end.
+stop_process(Proc, Reason) when is_pid(Proc) ->
+  catch exit(Proc, Reason);
+stop_process(undefined, _Reason) -> ok;
+stop_process(Proc, Reason) ->
+  Pid = whereis(Proc),
+  catch unregister(Proc),
+  stop_process(Pid, Reason).
