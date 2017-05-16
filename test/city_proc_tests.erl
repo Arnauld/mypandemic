af99222fe5a38e0d625edffee71c67f56d30a860
atrolla <yvan.vu@arolla.fr>
2017-05-16 09:32:15 +0200

    updated tests according to slides protocol

--- test/city_proc_tests.erl
+++ test/city_proc_tests.erl
@@ -6,7 +6,7 @@
 -include_lib("eunit/include/eunit.hrl").
 
 city_should_start_and_responds_to_infection_level_message__test() ->
-  {ok, Pid} = city_proc:start_link(paris, [london, essen]),
+  {ok, Pid} = city_proc:start(paris, [london, essen]),
   Pid ! {infection_level, blue, self()},
   receive
     Response ->
@@ -18,13 +18,13 @@ city_should_start_and_responds_to_infection_level_message__test() ->
 
 
 city_should_start_and_expose_its_infection_level__tes() ->
-  {ok, Pid} = city_proc:start_link(paris, [london, essen]),
+  {ok, Pid} = city_proc:start(paris, [london, essen]),
   Response = city_proc:infection_level(Pid, blue),
-  ?assertEqual({infection_level, paris, blue, 0}, Response).
+  ?assertEqual({paris, blue, 0}, Response).
 
 
 city_should_start_and_responds_to_infect_message__tes() ->
-  {ok, Pid} = city_proc:start_link(paris, [london, essen]),
+  {ok, Pid} = city_proc:start(paris, [london, essen]),
   Pid ! {infect, blue, self()},
   receive
     InfectionResult ->
@@ -36,13 +36,29 @@ city_should_start_and_responds_to_infect_message__tes() ->
 
 
 city_should_start_and_be_infected__tes() ->
-  {ok, Pid} = city_proc:start_link(paris, [london, essen]),
+  {ok, Pid} = city_proc:start(paris, [london, essen]),
   InfectionResult = city_proc:infect(Pid, blue),
   ?assertEqual({infected, paris, blue, 1}, InfectionResult).
 
-city_should_start_and_responds_to_infect_message_until_outbreak__tes() ->
-  {ok, Pid} = city_proc:start_link(paris, [london, essen]),
+%%
+%% replyTo(no_reply, _City, _Color, _Message) -> noreply;
+%% replyTo(From, City, Color, {Verb, Data}) ->
+%%   From ! {Verb, city:name(City), Color, Data}.
+city_should_start_and_increment_infection_to_infect_async_message__tes() ->
+  {ok, Pid} = city_proc:start(paris, [london, essen]),
   Pid ! {infect, blue}, %% async -> no reply
+  Response = city_proc:infection_level(Pid, blue),
+  ?assertEqual({paris, blue, 1}, Response).
+
+city_should_start_and_be_infected_async__tes() ->
+  {ok, Pid} = city_proc:start(paris, [london, essen]),
+  {infect, blue} = city_proc:infect_async(Pid, blue),
+  Response = city_proc:infection_level(Pid, blue),
+  ?assertEqual({paris, blue, 1}, Response).
+
+city_should_start_and_responds_to_infect_message_until_outbreak__tes() ->
+  {ok, Pid} = city_proc:start(paris, [london, essen]),
+  Pid ! {infect, blue},
   Pid ! {infect, blue},
   Pid ! {infect, blue},
   Pid ! {infect, blue, self()},
@@ -56,7 +72,7 @@ city_should_start_and_responds_to_infect_message_until_outbreak__tes() ->
 
 
 city_should_start_and_should_outbreak_after_4_infections__tes() ->
-  {ok, Pid} = city_proc:start_link(paris, [london, essen]),
+  {ok, Pid} = city_proc:start(paris, [london, essen]),
   city_proc:infect(Pid, blue),
   city_proc:infect(Pid, blue),
   city_proc:infect(Pid, blue),
@@ -65,12 +81,12 @@ city_should_start_and_should_outbreak_after_4_infections__tes() ->
 
 
 each_city_should_start_in_its_own_process__tes() ->
-  {ok, Pid1} = city_proc:start_link(paris, [london, essen]),
-  {ok, Pid2} = city_proc:start_link(paris, [london, essen]),
+  {ok, Pid1} = city_proc:start(paris, [london, essen]),
+  {ok, Pid2} = city_proc:start(paris, [london, essen]),
   ?assertNotEqual(Pid1, Pid2).
 
 city_should_start_and_stop_gracefully__tes() ->
-  {ok, Pid} = city_proc:start_link(paris, [london, essen]),
+  {ok, Pid} = city_proc:start(paris, [london, essen]),
   Pid ! stop,
   timer:sleep(50), %% unnecessary ?.
   ?assertEqual(false, is_process_alive(Pid)).
--- test/city_sup_tests.erl
+++ test/city_sup_tests.erl
@@ -5,80 +5,57 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-%% The idea here is to have one supervisor for all cities.
-%%
-%% Its responsibility is to spawn a city, monitor it,
-%% and restart it when the city fails (loosing the city state).
-%%
-%% If a city terminates normally,
-%% remove it from the monitored processes.
-%%
-%% If the last city terminates normally,
-%% the supervisor has no more reason to live :)
-%%
-%% supervisor must keep track of monitored cities references,
-%% so it can recreate them (Using Name and Neighbours data)
-
-%% register(atom_name, a_pid),
-supervisor_should_start_and_register_a_process__test() ->
-  unregister_city_sup(city_sup),
-  {ok, Pid} = city_sup:start_link(),
+%% HOW TO SPAWN A CHILD :
+%% spawnChild(Name, Neighbours) ->
+%%   {ok, Pid} = city_proc:start(Name, Neighbours),
+%%   register(Name, Pid),
+%%   monitor(process, Pid).
+supervisor_should_start_and_register_new_city_process__test() ->
+  {ok, Pid} = city_sup:start(paris, [london, essen]),
   timer:sleep(50),
-  ?assertEqual(Pid, whereis(city_sup)).
+  try
+    ?assertEqual({paris, blue, 0}, city_proc:infection_level(paris, blue))
+  after
+    io:format("0...~n"),
+    kill_city_sup(Pid, paris)
+  end.
 
-%% monitor(process, Pid) to monitor a process
-%% returns an identifier called Reference
-supervisor_should_register_the_new_city_process__tes() ->
-  supervise_paris_city(),
-  ?assertEqual(true, is_pid(whereis(paris))).
-
-supervise_paris_city() ->
-  unregister_city_sup(city_sup),
-  unregister_city_sup(paris),
-  city_sup:start_link(),
-  timer:sleep(50),
-  city_sup:new_child(paris, [london, essen]),
-  timer:sleep(50).
 
 %% a monitored process sends a message of the form :
-%% {'DOWN', Reference, process, Pid, Reason}
+%% {'DOWN', Ref, process, Pid, Reason}
 %% before terminating
-%% Reference = Identifier returned when monitor was called
 %% Reason = normal when it is not a failure
-supervisor_should_restart_the_city_process_after_a_failure__tes() ->
-  supervise_paris_city(),
-  ParisPid = whereis(paris),
-  paris ! {infection_level, blue, make_it_fail},
+supervisor_should_restart_the_city_process_after_a_failure__test() ->
+  {ok, Pid} = city_sup:start(london, [paris, essen]),
   timer:sleep(50),
-  RestartedParisPid = whereis(paris),
-  ?assertNotEqual(ParisPid, RestartedParisPid).
+  try
+    ?assertEqual({infected, london, blue, 1}, city_proc:infect(london, blue))
+  after
+    kill_city_sup(Pid, london)
+  end.
 
-supervisor_should_stop_when_its_last_child_stops_gracefully__tes() ->
-  supervise_paris_city(),
-  paris ! stop,
-  timer:sleep(50),
-  ?assertEqual(undefined, whereis(city_sup)).
+%% kill -> infections data should be lost
+%timer:sleep(50),
+%kill_city(london),
+%timer:sleep(50),
+%%  ?assertEqual({london, blue, 1}, city_proc:infection_level(london, blue)).
 
-supervisor_should_still_live_when_one_of_its_children_stops_gracefully__tes() ->
-  supervise_paris_city(),
-  unregister_city_sup(london),
-  city_sup:new_child(london, [paris, essen]),
+supervisor_should_stop_when_its_child_stops_gracefully__test() ->
+  {ok, Pid} = city_sup:start(essen, [london, paris]),
   timer:sleep(50),
-  paris ! stop,
+  essen ! stop,
   timer:sleep(50),
-  ?assertEqual(true, is_process_alive(whereis(city_sup))).
+  ?assertEqual(false, is_process_alive(Pid)).
 
-unregister_city_sup(ProcName) ->
-  case whereis(ProcName) of
-    undefined -> no_op;
-    _ -> case is_process_alive(whereis(ProcName)) of
-           true ->
-             case ProcName of
-               city_sup -> exit(whereis(city_sup), kill);
-               _ -> no_op
-             end,
-             unregister(ProcName);
-           _ ->
-             no_op
-         end
+kill_city_sup(Pid, City) ->
+  try
+    io:format("1...~n"),
+    exit(Pid, kill),
+    io:format("2...~n"),
+    try exit(whereis(City), kill)
+    catch _ ->
+      ignore end,
+    io:format("3...~n")
+  catch _ ->
+    ignore
   end.
